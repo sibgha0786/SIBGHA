4db2e753211f152747f6a3616c494a75be8198cf
sibgha0786 <manohunny477@gmail.com>
2025-08-13 09:28:08 +0500

    Update menu-new.js

--- plugins/menu-new.js
+++ plugins/menu-new.js
@@ -39,7 +39,7 @@ cmd({
             forwardingScore: 999,
             isForwarded: true,
             forwardedNewsletterMessageInfo: {
-                newsletterJid: '120363348739987203@newsletter',
+                newsletterJid: '923280355660@s.whatsapp.net',
                 newsletterName: config.OWNER_NAME,
                 serverMessageId: 143
             }
