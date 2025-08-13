bc06d55724ab71eda121d097d112e3ed03af61c3
sibgha0786 <manohunny477@gmail.com>
2025-08-13 09:28:50 +0500

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -41,8 +41,8 @@ async (conn, mek, m, { from, quoted, sender, reply }) => {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363348739987203@newsletter',
-                    newsletterName: "𝐀𝐫𝐬𝐥𝐚𝐧_𝐌𝐃",
+                    newsletterJid: '923280355660@s.whatsapp.net',
+                    newsletterName: "SIBGHA",
                     serverMessageId: 143
                 }
             }
